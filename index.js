95c36594ac27f5eaf033826e700fa8188ff0424c
Vivek Parashar <54675532+jdvivek@users.noreply.github.com>
2020-01-16 20:35:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var request = require("request");
+
+var app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+app.get("/", function(req, res) {
+    request({
+            uri: req.query.url
+        },
+        function(error, response, body) {
+            // console.log(body);
+            // res.send(process.cwd());
+
+            var d = body.replace('<script data-cfasync="false" type="text/javascript" src="//cryaptall.club/r7F0yM71eQjT4WZs/15574"></script>', '');
+
+            d = d.replace('<script type="text/javascript" async="" src="https://www.gstatic.com/recaptcha/releases/eQmzkx3d5dtuXlLOA4pEID3I/recaptcha__en.js"></script>', '');
+
+
+
+            res.status(200).send(d);
+        }
+    );
+});
+app.get("/lion", function(req, res) {
+    res.sendFile(process.cwd() + '/lion.html');
+});
+app.get("/lion.js", function(req, res) {
+    res.sendFile(process.cwd() + '/lion.js');
+});
+
+
+var server = app.listen(8081, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port);
+});
---
+++ lion.js
@@ -0,0 +1,62 @@
+/*VARIABLES*/
+
+//set frame id
+var movieframeId='myframe';
+
+
+/*Html function and events*/
+
+
+function onMyFrameLoad(frame) {
+
+  //clear console
+   console.clear();
+   console.log(frame);
+   //remove links
+RemoveLinksOpenInAnotherTab();
+//make visible
+document.getElementById("myframe").style.display="block";
+setInterval(function(){ 
+  
+   window['FWVc9MragE']=false; 
+   
+   }, 500);
+
+
+};
+
+
+/*UTLITY FUNCTIONS*/
+
+//Get Iframe 
+function GetMovieIframeDocument()
+{
+  //myframe is
+  var movieFrame=document.getElementById(movieframeId);
+	var doc =movieFrame.contentDocument? movieFrame.contentDocument: movieFrame.contentWindow.document;
+	return doc;
+}
+//Remove Link From Iframe
+window.RemoveLinksOpenInAnotherTab=function ()
+{
+  var movieDocument=GetMovieIframeDocument();
+
+ $(movieDocument.documentElement).click(function(){
+   
+   RemoveLinksOpenInAnotherTab();
+    
+    
+  });
+
+	var alllink=movieDocument.getElementsByTagName("a");
+	for(var x=0; x<alllink.length; x++)
+	{
+		if(alllink[x].getAttribute("target")=="_blank")
+		{
+     
+      alllink[x].remove();
+    
+		 // alllink[x].setAttribute("href","#");
+		}	
+  }
+}
